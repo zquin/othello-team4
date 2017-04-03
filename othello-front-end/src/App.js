4942dfa4ac8a8830a8716b8eec0785dc1cc79516
jpease1020 <justinpease2@gmail.com>
2017-04-03 15:48:18 -0400

    Add registration page

--- othello-front-end/src/App.js
+++ othello-front-end/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {FormField, Form} from 'react-pattern-library';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>THE APP HAS BEEN IMPORTED</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form>
+          <FormField
+            id="email-box"
+            label="email"
+            required />
+          <FormField
+            id="password-box"
+            label="password"
+            required />
+          <button className="c-btn c-btn--secondary" id="register-button">Register</button>
+        </Form>
       </div>
     );
   }
--- othello-front-end/src/App.test.js
+++ othello-front-end/src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders the registration page', () => {
+  const div = document.createElement('div');
+  const app = shallow(<App />, div);
+
+  expect(app.find('#email-box')).toHaveLength(1);
+  expect(app.find('#password-box')).toHaveLength(1);
+  expect(app.find('#register-button')).toHaveLength(1);
 });
