e575a36b31f78ccfd5a1d7ca6e74b63c0d53e16a
jpease1020 <justinpease2@gmail.com>
2017-04-05 13:38:38 -0400

    Add Gameboard, need to add functionality to flip the color of the chip on click

--- othello-front-end/src/App.js
+++ othello-front-end/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import {FormField, Form} from 'react-pattern-library';
 import './App.css';
 import RegisterUser from "./RegisterUser";
+import GameBoard from "./GameBoard";
 //import BusinessLogic from "./BusinessLogic";
 
 
@@ -15,9 +16,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Wired
 
         <RegisterUser />
+          <GameBoard />
       </div>
     );
   }
