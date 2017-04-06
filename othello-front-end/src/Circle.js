f348b506d70a900e15d13255758f874e83538c4b
jpease1020 <justinpease2@gmail.com>
2017-04-06 08:22:21 -0400

    Update Circle and Row

--- othello-front-end/src/Circle.js
+++ othello-front-end/src/Circle.js
@@ -3,29 +3,19 @@ import './App.css';
 
 export default class Circle extends Component
 {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            color: "rgba(0,0,0,0.1)"
-        }
-    }
-
-    get flipBlack() {
-        let tempstate = this.state;
-        this.setState(tempstate.color="rgba(0,0,0,1)")
-    }
-
-    get flipWhite() {
-        let tempstate = this.state;
-        this.setState(tempstate.color="rgba(255,255,255, 0.9)")
-    }
+    // constructor(props) {
+    //     super(props)
+    //
+    //     this.state = {
+    //         color: this.props.color
+    //     }
+    // }
 
     render() {
         return (
-        <svg height="100" width="100">
-            <circle cx="50" cy="50" r="40" fill={this.state.color}/>
-        </svg>);
+            <div className={this.props.color}>
+            </div>
+        );
     }
 
 }
--- othello-front-end/src/Row.js
+++ othello-front-end/src/Row.js
@@ -1,34 +1,52 @@
 import React, {Component} from 'react';
 import './App.css';
 import Circle from "./Circle";
-export default  class Row extends Component {
+export default class Row extends Component {
 
     constructor(props) {
         super(props);
 
-        this.circle = this.circle.bind(this);
+        this.changeColor = this.changeColor.bind(this);
+        this.state = {
+            color: "",
+            blacksTurn: true
+        }
     }
 
-    circle ()
-    {
-        console.log ("We are here")
+    changeColor () {
+        let state = this.state
+        console.log(state.color);
+        if (this.state.blacksTurn) {
+            state.color="circleBlack"
+        } else {
+            state.color="circleGrey"
+        }
 
-        // we need to change the state (color) here I think
+        state.blacksTurn = !this.state.blacksTurn
 
-        return (<Circle color="black"/>)
-
+        this.setState(state)
     }
+
+    //
+    // flipBlack () {
+    //     this.setState(tempstate.color="rgba(0,0,0,1)")
+    // }
+    //
+    // flipWhite () {
+    //     this.setState(tempstate.color="rgba(255,255,255, 0.9)")
+    // }
+
     render() {
         return (
             <div id="rowComponent">
-                <div id="cell0" onClick={this.circle}><Circle/></div>
-                <div id="cell1" onClick={this.circle}><Circle/></div>
-                <div id="cell2" onClick={this.circle}><Circle/></div>
-                <div id="cell3" onClick={this.circle}><Circle/></div>
-                <div id="cell4" onClick={this.circle}><Circle/></div>
-                <div id="cell5" onClick={this.circle}><Circle/></div>
-                <div id="cell6" onClick={this.circle}><Circle/></div>
-                <div id="cell7" onClick={this.circle}><Circle/></div>
+                <div id="cell0" onClick={this.changeColor}><Circle color={this.state.color}/></div>
+                <div id="cell1" onClick={this.changeColor}><Circle/></div>
+                <div id="cell2" onClick={this.changeColor}><Circle/></div>
+                <div id="cell3" onClick={this.changeColor}><Circle/></div>
+                <div id="cell4" onClick={this.changeColor}><Circle/></div>
+                <div id="cell5" onClick={this.changeColor}><Circle/></div>
+                <div id="cell6" onClick={this.changeColor}><Circle/></div>
+                <div id="cell7" onClick={this.changeColor}><Circle/></div>
             </div>
         )
     }
